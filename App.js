f6dd111b52202e6feabce5d77fb855e8f1093c61
mankkk208 <91036546+mankkk208@users.noreply.github.com>
2024-06-13 16:08:26 +0700

    commit app to work with labelManager

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Labels from './screens/Labels';
 import Home from "./screens/Home.js";
 import NewNote from "./screens/NewNote.js";
 import EditNote from './screens/EditNote.js';
-import ManageLabels from './screens/ManageLabels.js'
-import Labels from './screens/Labels.js'
+import ManageLabels from './screens/ManageLabels';
+import LabelManager from './components/LabelManager'; // Make sure to import LabelManager
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="New note" component={NewNote}/>
-        <Stack.Screen name="Edit note" component={EditNote}/>
-        <Stack.Screen name="Labels" component={Labels}/>
-        <Stack.Screen name="Manage labels" component={ManageLabels}/>
-      </Stack.Navigator>
+      <LabelManager>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="New note" component={NewNote} />
+          <Stack.Screen name="Edit note" component={EditNote} /> 
+          <Stack.Screen name="Labels" component={Labels} />
+          <Stack.Screen name="Manage labels" component={ManageLabels} />
+        </Stack.Navigator>
+      </LabelManager>
     </NavigationContainer>
   );
 }
@@ -27,7 +31,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
